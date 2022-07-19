af671ff794d86948cbe37a165fdbee9ef3b31a28
marinadedev <marinadegames@gmail.com>
2022-07-19 22:26:39 +0300

    added 'closure' script example

--- scripts/06_closure.js
+++ scripts/06_closure.js
@@ -3,42 +3,12 @@
 
 function sayHelloTo(name) {
     const message = 'Hello, ' + name
-
     return function () {
         console.log(message)
     }
 }
 
-// const sayHelloToEugene = sayHelloTo("Eugene")
-// const sayHelloToElina = sayHelloTo("Elina")
-// sayHelloTo('Eugene')()
-// sayHelloToElina()
-
-function createFrameworkManager() {
-    const fw = ['Angular', 'React']
-
-    return {
-        print: function() {
-            console.log(fw.join(' '))
-        },
-        add: function (framework){
-            fw.push(framework)
-        }
-    }
-}
-
-// const manager = createFrameworkManager()
-// console.log(manager)
-// manager.print()
-// manager.add('Vue.js')
-// manager.print()
+const helloToElina = sayHelloTo('Elina')
+const helloToEugene = sayHelloTo('Eugene')
 
 
-// ====================================
-const fib = [1,2,3,5,8,13]
-
-for (let i = 0; i <fib.length ; i++) {
-    setTimeout(() => {
-        console.log(`fib[${i}] = ${fib[i]}`)
-    }, 1500)
-}
